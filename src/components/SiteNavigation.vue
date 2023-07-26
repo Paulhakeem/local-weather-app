6d9ea0995e305d28764351bc7560d6c67d2a5676
Paulhakeem <paulnyamawi18@gmail.com>
2023-07-26 21:47:15 +0300

    push to production

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+import BaseModal from './BaseModal.vue';
+
+const modalActive = ref(null)
+const toggleModal = () => {
+modalActive.value = !modalActive.value
+}
+</script>
+<template>
+ <header class=" shadow-lg bg-cover">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+      <RouterLink :to="{name: 'home'}">
+        <div class="flex items-center gap-3">
+        <i class="fa-solid fa-sun text-2xl"></i>
+        <p class="text-2xl">The Local Weather App</p>
+      </div>
+      </RouterLink>
+      <div class="flex gap-3 flex-1 justify-end">
+        <i @click="toggleModal"
+        class="fa-solid fa-circle-info text-2xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        <i class="fa-solid fa-plus text-2xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+      </div>
+
+      <BaseModal 
+      :modalActive="modalActive"
+      @close-modal="toggleModal">
+      <div class="text-black">
+      <h2 class="text-xl font-semibold mb-1">About:</h2>
+      <p class="mb-4">
+        The local weather allows you to track the current
+        and the future weather of the cities you choose
+        </p>
+        <h2 class="text-xl font-semibold mb-1">How it works:</h2>
+        <ol class="list-decimal list-inside mb-4">
+        <li>
+          Search your city by entering the name into the search bar.
+        </li>
+        <li>
+          Select a city within the result, this will take you to the current weather for your selection.
+        </li>
+        <li>
+          track the city by clicking on the plus icon " + " in the top right.
+          This will save the city to view at a later time on the home page.
+        </li>
+        </ol>
+        <h2 class="text-xl font-semibold mb-1">How to Remove a city:</h2>
+      </div>
+      </BaseModal>
+    
+    </nav>
+ </header>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,141 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const input = ref("")
+const cities = ref([])
+
+
+// Get a list of cities that match the user's input
+    const getCities = async (city) => {
+       try{
+        const response = await fetch
+        (`https://api.openweathermap.org/data/2.5/find?q=${city}&appid=f2eca4d4b657ee27eac21b7a41a0b4aa`)
+        const {list} = await response.json()
+        
+        if(list) {
+        cities.value = list
+        console.log(cities.value)
+        }
+       //input.value = ""
+       }
+       catch(err) {
+        console.log(err.message)
+
+       }
+    }
+    watch(input, (newValue, _) => {
+        if(newValue) {
+            getCities(newValue)
+        }
+    })
+   
+</script>
+<template>
+    <main class="container text-white">
+        <div class="pt-4 mb-8 relative">
+            <div
+            class="flex">
+            <input
+            v-model="input"
+            @keyup.enter="getCities"
+            type="text" 
+            placeholder="search for city or state"
+            class="py-2 px-1 w-full bg-transparent border-b 
+            focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+            </div>
+            
+            
+            <ul
+                class="absolute text-white
+                w-full py-2 top-[66px] bg-weather-secondary text-center items-center">
+               
+                <li
+                v-for="city in cities" 
+                :key="city.id">
+                <p class="justify-center text-2xl font-semibold ml-2 mr-2 pt-6">
+                    <i class="fa-solid fa-location-dot fa-1x"></i> 
+                    Today's Highlights in {{ city.name }}, {{ city.sys.country }}
+                </p>
+                 <div 
+                 class="gap-6 pt-6 pl-2 mb-6 grid grid-cols-2 items-center sm:grid-cols-2 md:grid-cols-4 ml-4 mr-4"> 
+                    <div class="bg-weather-primary shadow-lg rounded-md px-4 inline-grid">
+                        <h3 class="text-center py-6 text-lg">Temperature</h3>
+                        <i class="fa-solid fa-temperature-low fa-7x text-white mb-4"></i>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-3xl">{{ Math.round(city.main.temp) - 273}}</span>&#176C
+                    </p>
+                    </div>
+                   
+                    <div  class="bg-weather-primary shadow-xl rounded-md px-4 inline-grid">
+                    <h3 class="text-center py-6 text-lg">Wind Speed</h3>
+                    <i class="fa-solid fa-wind fa-7x mb-4 text-white"></i>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-3xl">{{ city.wind.speed}}</span>m/s 
+                    </p>
+                    </div>
+
+                    
+                    <div class="bg-weather-primary shadow-lg rounded-md px-4 inline-grid">
+                    <h3 class="text-center py-6 text-lg">Rainfall (cubic)</h3>
+                    <i class="fa-solid fa-cloud-showers-heavy fa-7x pb-4" 
+                    style="color: #ffffff;"></i>
+                    <p class="text-xl text-center mb-4">
+                       <span v-if="city.rain" class="text-3xl">{{ city?.rain["1h"] }}m&#x33a5</span>
+                       <span v-else>No Rain</span>
+                    </p>
+                   
+                    </div>
+
+
+                    <div  class="bg-weather-primary shadow-lg rounded-md px-4 inline-grid">
+                    <h3 class="text-center py-6 text-lg">Feels Like</h3>
+                    <i class="fa-solid fa-face-grin-beam-sweat fa-7x text-white mb-4"></i>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-3xl">{{ Math.round(city.main.temp) - 273}}</span>
+                        &degC
+                    </p>
+                    </div>
+
+                    <div  class="bg-weather-primary shadow-lg rounded-md px-4 inline-grid">
+                    <h3 class="text-center py-6 text-lg">Humidity</h3>
+                    <i class="fa-solid fa-droplet text-white fa-7x mb-4"></i>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-3xl">{{ city.main.humidity}}%</span>
+                    </p>
+                    </div>
+
+
+                    <div  class="bg-weather-primary shadow-lg rounded-md px-4 inline-grid h-64">
+                    <h3 class="text-center py-6 text-lg">Clouds</h3>
+                    <i class="fa-solid fa-cloud text-white fa-6x mb-4"></i>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-2xl">{{ city.weather[0].description }}</span>
+                    </p>
+                    </div>
+
+
+                    <div  class="px-4 inline-grid pl-16 w-64">
+                    <h3 class="text-center py-6 text-lg">Coordinates</h3>
+                    <i class="fa-solid fa-earth-africa fa-10x mb-4"></i>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-xl">Latitude: {{ city.coord.lat }}</span>
+                    </p>
+                    <p class="text-xl text-center mb-4">
+                        <span class="text-xl">Longitude: {{ city.coord.lon}}</span>
+                    </p>
+                    </div>
+                 </div>
+                </li>
+                <div class="bg-gradient-to-r from-weather-primary to-weather-secondary rounded-6xl">
+                    <p class="font-light">Designed With 
+                    <i class="fa-solid fa-heart-circle-check text-red-500 text-xl py-2 "></i>
+                    By CodeWithPaul
+                </p>
+                </div>
+                
+            </ul>
+        </div>
+
+ 
+    </main>
+</template>
